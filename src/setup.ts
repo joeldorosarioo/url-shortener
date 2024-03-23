51e580617c19a36416d06b28cc2f96198ad0834f
Joel Oliveira <joeldorosarioo@gmail.com>
2024-03-22 23:40:05 -0300

    feat: create the 'url_shortener' table in the postgresql database

---
+++ src/setup.ts
@@ -0,0 +1,17 @@
+import { sql } from './databases/postgres';
+
+// SQL code to create url_shortener table into Postgres database.
+
+(async () => {
+  await sql`
+		CREATE TABLE IF NOT EXISTS url_shortener (
+			id SERIAL PRIMARY KEY,
+			code TEXT UNIQUE,
+			original_url TEXT,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);
+	`
+
+	await sql.end() // Wait until the end of the execution.
+	console.log('Setup successfully completed!');
+})();
